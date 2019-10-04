a72d5dda0a761d16088bf6ff1e77436c98bdd3c4
Julian Mina Caicedo <julianandres.mina@experis-spain.com>
2019-10-04 15:30:37 +0200

    Adding a route for processing bulk invoices

--- invoice-publisher/src/main/java/com/trainingsession/invoicepublisher/route/PublishingRoute.java
+++ invoice-publisher/src/main/java/com/trainingsession/invoicepublisher/route/PublishingRoute.java
@@ -6,6 +6,7 @@ import javax.annotation.Resource;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.ProcessorDefinition;
 import org.apache.camel.model.RouteDefinition;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.apache.camel.model.rest.RestDefinition;
@@ -19,6 +20,12 @@ import org.springframework.stereotype.Component;
 public class PublishingRoute extends RouteBuilder {
 
   private static final Logger logger = LoggerFactory.getLogger(PublishingRoute.class);
+  private static final String BULK_ENDPOINT_PATH = "/invoice/bulk";
+  private static final String BULK_ENDPOINT_DESCRIPTION = "Post a bunch of invoices at one time";
+  private static final String BULK_BODY_DESCRIPTION = "A string containing a list of invoices";
+  private static final String INVOICE_ENDPOINT_PATH = "/invoice";
+  private static final String INVOICE_ENDPOINT_DESCRIPTION = "Post an invoice";
+  private static final String INVOICE_BODY_DESCRIPTION = "A string containing the information of the invoice";
 
   @Value("${server.port}")
   private int port;
@@ -34,7 +41,6 @@ public class PublishingRoute extends RouteBuilder {
 
   @Override
   public void configure() throws Exception {
-
     // Configureation camel
     restConfiguration()
         .component("netty4-http")
@@ -46,18 +52,16 @@ public class PublishingRoute extends RouteBuilder {
         .apiProperty("api.title", "Invoice Publisher API")
         .apiProperty("api.version", "1.0.0")
         .apiProperty("cors", "true");
+    addInvoiceRoute();
+    addInvoiceBulkRoute();
+  }
 
-    // Configure the route entry
-    RestDefinition restDef = rest("/api")
-        .post("/invoice")
-        .consumes("text/plain")
-        .bindingMode(RestBindingMode.off)
-        .description("Post an invoice")
-        .produces("application/json")
-        .outType(String.class);
-    addFilters(restDef);
-
-    // configure the route to process the Cohesion Post
+  private void addInvoiceRoute() {
+    RestDefinition restDef = createRestDefinition(
+        INVOICE_ENDPOINT_PATH,
+        INVOICE_ENDPOINT_DESCRIPTION,
+        INVOICE_BODY_DESCRIPTION);
+    // configure the route to process a single invoice
     RouteDefinition routeDefinition = restDef.route()
         .routeId("InvoicePublisherRoute")
         .log("Preparing the invoice to be published")
@@ -65,17 +69,39 @@ public class PublishingRoute extends RouteBuilder {
         .to(getKafkaConfig())
         .log("Invoice was successfully published to Kafka");
     addExceptionHandler(routeDefinition);
-
   }
 
-  private void addFilters(RestDefinition restDef){
+  private void addInvoiceBulkRoute() {
+   RestDefinition restDef = createRestDefinition(
+       BULK_ENDPOINT_PATH,
+       BULK_ENDPOINT_DESCRIPTION,
+       BULK_BODY_DESCRIPTION);
+    // configure the route to process a bunch of invoices
+    ProcessorDefinition<?> definition = restDef.route()
+        .routeId("InvoiceBulkPublisherRoute")
+        .split(body().tokenize("\n")).streaming()
+        .log("Preparing the invoices to be published")
+        .process(processor)
+        .to(getKafkaConfig())
+        .log("Invoice was successfully published to Kafka")
+        .end();
+    addExceptionHandler(definition);
+  }
+
+  private RestDefinition createRestDefinition(String endpointPath, String endpointDescription, String bodyDescription) {
+    RestDefinition restDef = rest("/api")
+        .post(endpointPath)
+        .consumes("text/plain")
+        .bindingMode(RestBindingMode.off)
+        .description(endpointDescription)
+        .produces("application/json")
+        .outType(String.class);
+
+    // Swagger configuration
     restDef.param()
-        .name("body")
-        .type(body)
-        .description("A string containing the information of the invoice")
-        .dataType("string")
-        .required(true)
-        .endParam();
+        .name("body").type(body).description(bodyDescription)
+        .dataType("string").required(true).endParam();
+    return restDef;
   }
 
   private String getKafkaConfig(){
@@ -84,7 +110,7 @@ public class PublishingRoute extends RouteBuilder {
         + "&serializerClass=" + StringSerializer.class.getName();
   }
 
-  private void addExceptionHandler(RouteDefinition route) {
+  private void addExceptionHandler(ProcessorDefinition<?> route) {
     route.onException(Exception.class).handled(true)
         .process(exchange -> {
           Exception ex = (Exception)exchange.getProperty(Exchange.EXCEPTION_CAUGHT);
